49bb3fff6420e76b6451957b62d47cc35e273f5b
atomr <atomr@MacBook-Air-atomr.local>
2020-04-25 16:26:11 +0500

    Updates

---
+++ precache-manifest.f583b47ba06c9603c8c262e877f3f711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d193a9893c66d3224be2b43672b8830",
+    "url": "/react-filtred-products/index.html"
+  },
+  {
+    "revision": "0b5450c3716a7ee279fd",
+    "url": "/react-filtred-products/static/css/main.36e60718.chunk.css"
+  },
+  {
+    "revision": "65f47cd9266617d37024",
+    "url": "/react-filtred-products/static/js/2.07a67af3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-filtred-products/static/js/2.07a67af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19902d0fc130d806597",
+    "url": "/react-filtred-products/static/js/3.741a500e.chunk.js"
+  },
+  {
+    "revision": "0b5450c3716a7ee279fd",
+    "url": "/react-filtred-products/static/js/main.bab71b83.chunk.js"
+  },
+  {
+    "revision": "04921e13a9c5e3d10015",
+    "url": "/react-filtred-products/static/js/runtime-main.c728efa7.js"
+  }
+]);
